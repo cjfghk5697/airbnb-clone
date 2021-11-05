d0e5200a38fff490d04f719355b93bbce3fcaeb4
cjfghk5697 <cjfghk5697@gmail.com>
2021-11-05 20:18:03 +0900

    final user model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 # Register your models here.
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display=('username', 'gender','language','currency','superhost')
+class CustomUserAdmin(UserAdmin):
+    '''
+    Custorm User Admin
+    '''
+    fieldsets = UserAdmin.fieldsets + (
+        ("Custom Profile",{"fields":("avatar","gender","bio","birthdate","language","currency","superhost",),},),)
+    
