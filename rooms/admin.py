9d696804a6761020c04a84fc121c5b52e46b780f
cjfghk5697 <cjfghk5697@gmail.com>
2021-11-08 19:45:05 +0900

    Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.options import HORIZONTAL
 from . import models
 
 # Register your models here.
@@ -16,7 +17,67 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths",)})(
+            "Basic Info",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "address",
+                    "price",
+                )
+            },
+        ),
+        (
+            "Times",
+            {
+                "fields": (
+                    "check_in",
+                    "check_out",
+                    "instant_book",
+                )
+            },
+        ),
+        (
+            "More About the Space",
+            {
+                "fields": (
+                    "amenities",
+                    "facilities",
+                    "house_rules",
+                )
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+    list_disply = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+    list_filter = (
+        "isntant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilites",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_filter = ("=city", "^host__username")
+    filter_horizontal = ("amenities", "facilities", "house_rules")
 
 
 @admin.register(models.Photo)
