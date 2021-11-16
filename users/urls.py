5d728e70d342281a18db8689a7c1c80aa5d95a57
cjfghk5697 <cjfghk5697@gmail.com>
2021-11-16 11:15:01 +0900

    signup

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = "users"
 urlpartters = [
     path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.logout, name="logout"),
+    path("signup", views.SignUpView.as_view, name="sign up"),
 ]
--- users/views.py
+++ users/views.py
@@ -24,3 +24,26 @@ class LoginView(FormView):
 def log_out(self):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    temlplate_name = "users/signup.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "길동",
+        "last_name": "홍",
+        "email": "itn@las.com",
+        "password": "1234",
+        "password1": "1234",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authentiacate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+        return super().form_valid(form)
